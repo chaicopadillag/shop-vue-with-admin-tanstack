61a172f3ceafa0daf976d123a8e78f6777b801d0
chaicopadillag <xt9738@rimac.com.pe>
2025-01-11 07:44:44 -0500

    add request interceptor to shopApi; include authorization token in headers

--- src/api/shopApi.ts
+++ src/api/shopApi.ts
@@ -1,10 +1,18 @@
-import axios from 'axios';
+import axios, { type InternalAxiosRequestConfig } from 'axios';
 
 const shopApi = axios.create({
   baseURL: import.meta.env.VITE_SHOP_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
+});
+
+shopApi.interceptors.request.use((config: InternalAxiosRequestConfig) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export { shopApi };
