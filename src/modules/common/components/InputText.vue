34095be16ec196d9a02bf9b5c9f31f54f1867fac
chaicopadillag <xt9738@rimac.com.pe>
2025-01-11 21:21:07 -0500

    add InputText and TextArea components; implement reusable input fields with validation and styling

---
+++ src/modules/common/components/InputText.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <input
+      :type="type"
+      :value="modelValue"
+      @input="$emit('update:modelValue', ($event.target as HTMLInputElement)?.value ?? '')"
+      @blur="$emit('blur')"
+      :placeholder="placeholder"
+      :class="['form-control', { 'border-red-500': error }]"
+    />
+    <span v-if="error" class="text-red-500 text-xs">{{ error }}</span>
+  </div>
+</template>
+
+<script lang="ts" setup>
+type Props = {
+  modelValue?: string;
+  placeholder?: string;
+  error?: string;
+  type?: 'text' | 'email' | 'password' | 'number';
+};
+
+withDefaults(defineProps<Props>(), {
+  type: 'text',
+});
+
+defineEmits(['update:modelValue', 'blur']);
+</script>
+
+<style scoped>
+.form-control {
+  @apply shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none dark:bg-gray-700 dark:text-white;
+}
+</style>
---
+++ src/modules/common/components/TextArea.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <textarea
+      :value="modelValue"
+      @input="$emit('update:modelValue', ($event.target as HTMLTextAreaElement)?.value ?? '')"
+      @blur="$emit('blur')"
+      :placeholder="placeholder"
+      :class="[
+        'shadow h-32 appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline',
+        { 'border-red-500': error },
+      ]"
+    ></textarea>
+    <span v-if="error" class="text-red-500 text-xs">{{ error }}</span>
+  </div>
+</template>
+
+<script lang="ts" setup>
+type Props = {
+  modelValue?: string;
+  placeholder?: string;
+  error?: string;
+};
+
+defineProps<Props>();
+
+defineEmits(['update:modelValue', 'blur']);
+</script>
+
+<style scoped>
+.form-control {
+  @apply shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none dark:bg-gray-700 dark:text-white;
+}
+</style>
