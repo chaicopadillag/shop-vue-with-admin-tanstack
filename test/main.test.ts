76a9fa3e067f140e4467a7a81bd478f65d59a532
chaicopadillag <xt9738@rimac.com.pe>
2025-01-13 10:48:53 -0500

    add @pinia/testing dependency and enhance main app tests; mock dependencies and verify app mounting logic

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,5 +1,48 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-require-imports */
+
+vi.mock('@tanstack/vue-query', () => ({
+  VueQueryPlugin: 'VueQueryPlugin',
+}));
+
+vi.mock('@/router', () => ({
+  default: 'router',
+}));
+
+vi.mock('vue-toastification', () => ({
+  default: 'Toast',
+}));
+
+vi.mock('pinia', async (pinia) => {
+  const mockPinia: any = await pinia();
+  return {
+    ...mockPinia,
+    createPinia: vi.fn(() => 'pinia'),
+  };
+});
+
 describe('main app', () => {
-  it('should pass', () => {
-    expect(true).toBe(true);
+  const vue = require('vue');
+
+  const useSpy = vi.fn();
+  const mountSpy = vi.fn();
+
+  const createAppSpy = vi.fn(() => ({
+    use: useSpy,
+    mount: mountSpy,
+  }));
+
+  vue.createApp = createAppSpy;
+
+  it('should mount the app', async () => {
+    await import('../src/main');
+    expect(useSpy).toBeCalledTimes(4);
+    expect(useSpy).toBeCalledWith('VueQueryPlugin');
+    expect(useSpy).toBeCalledWith('pinia');
+    expect(useSpy).toBeCalledWith('router');
+    expect(useSpy).toBeCalledWith('Toast');
+
+    expect(createAppSpy).toBeCalledTimes(1);
+    expect(mountSpy).toBeCalledWith('#app');
   });
 });
