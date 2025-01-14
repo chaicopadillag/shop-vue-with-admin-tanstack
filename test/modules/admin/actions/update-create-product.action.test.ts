0b6043a5a5640e46bfc13daf177b8beba595a2a0
chaicopadillag <xt9738@rimac.com.pe>
2025-01-14 09:22:21 -0500

    add unit tests for updateOrCreateProduct action; verify file upload functionality and API error handling

--- test/modules/admin/actions/update-create-product.action.test.ts
+++ test/modules/admin/actions/update-create-product.action.test.ts
@@ -1,4 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import fs from 'fs';
+import path from 'path';
+
 import { shopApi } from '@/api/shopApi';
 import { updateOrCreateProduct } from '@/modules/admin/actions/update-create-product.action';
 import AxiosMockAdapter from 'axios-mock-adapter';
@@ -88,6 +91,67 @@ describe('Update Product', () => {
     apiShopMock.onPatch(`/products/${updatePayload.id}`).reply(() => {
       throw new Error('Unit test error call api update product');
     });
+
+    await expect(updateOrCreateProduct(updatePayload)).rejects.toThrow('Error updating product');
+  });
+});
+
+describe('Subida Archivo', () => {
+  const filePath = path.join(__dirname, '../../../mock/t-shirt.jpg');
+  const fileImage = new File([fs.readFileSync(filePath)], 't-shirt.jpg', { type: 'image/jpeg' });
+
+  test('Debe de Subir Subir archivo cuando actualiza o se crea un producto', async () => {
+    const productUpdatePayload: any = {
+      id: '9de8ebd4-5cb3-4b5f-a2c9-eaa3a2a13dd1',
+      ...productPayload,
+      images: [...productPayload.images, fileImage],
+    };
+
+    const mockUploadFile = {
+      secureUrl: `${import.meta.env.VITE_SHOP_API_URL}/files/product/85e9cb09-bd34-4a19-b06f-ab3facbaaa4d.jpg`,
+    };
+
+    const productMock = {
+      ...productPayload,
+      title: 'Rustic Concrete Table',
+      price: 200,
+      description:
+        'New range of formal shirts are designed keeping you in mind. With fits and styling that will make you stand apart',
+      stock: 120,
+      images: [
+        `${import.meta.env.VITE_SHOP_API_URL}/files/product/1740176-00-C_0_2000.jpg`,
+        `${import.meta.env.VITE_SHOP_API_URL}/files/product/1740176-00-B_1.jpg`,
+        mockUploadFile.secureUrl,
+      ],
+      user: {
+        id: '568bea9d-66a6-4800-8823-c0fa02d95284',
+        email: 'test1@google.com',
+        fullName: 'Test One',
+        isActive: true,
+        roles: ['admin'],
+      },
+    };
+    apiShopMock.onPost(`/files/product`).reply(200, mockUploadFile);
+
+    apiShopMock.onPatch(`/products/${productUpdatePayload.id}`).reply(200, productMock);
+
+    const product = await updateOrCreateProduct(productUpdatePayload);
+
+    expect(product).toStrictEqual(productMock);
+    expect(product.images.pop()).toStrictEqual(mockUploadFile.secureUrl);
+  });
+
+  test('Debe de Prevenir el ERROR del API de Subida de Archivo del producto', async () => {
+    const updatePayload: any = {
+      id: '9de8ebd4-5cb3-4b5f-a2c9-eaa3a2a13dd1',
+      ...productPayload,
+    };
+
+    apiShopMock.onPost(`/files/product`).reply(() => {
+      throw new Error('Unit Test error in service upload file image');
+    });
+
+    apiShopMock.onPatch(`/products/${updatePayload.id}`).reply(200, {});
 
     await expect(updateOrCreateProduct(updatePayload)).rejects.toThrow('Error updating product');
   });
